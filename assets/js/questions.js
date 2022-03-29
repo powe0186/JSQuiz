1748e2fb1a3e74870893faa57250a55d6fa2a667
Ben Powell <powe0186@gmail.com>
2022-03-28 23:05:02 -0500

    questions load now. timer goes down on wrong answers.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,28 +1,85 @@
 var questionList = [
     {
         question: "JavaScript is considered a _________ language.",
-        option1: "typed",
-        option2: "typical",
-        option3: "basic",
-        option4: "loosely typed",
-        answer: "loosely typed"
+        a: "typed",
+        b: "typical",
+        c: "basic",
+        d: "loosely typed",
+        answer: "d"
     },
 
     {
         question: "Which of the following is true of JavaScript?",
-        option1: "It must be compiled",
-        option2: "It is a powerful tool for creating dynamic webpages.",
-        option3: "It only works in a browser.",
-        option4: "It is basically the same as Java.",
-        answer: "It is a powerful tool for creating dynamic webpages."
+        a: "It must be compiled",
+        b: "It is a powerful tool for creating dynamic webpages.",
+        c: "It only works in a browser.",
+        d: "It is basically the same as Java.",
+        answer: "b"
     },
 
     {
         question: "Who created JavaScript?",
-        option1: "Bill Gates.",
-        option2: "Brendan Eich.",
-        option3: "A group of 100 programers at Google.",
-        option4: "According to Genesis, God breathed it in to live on the 4th day.",
-        answer: "Brendan Eich."
+        a: "Bill Gates.",
+        b: "Brendan Eich.",
+        c: "A group of 100 programers at Google.",
+        d: "According to Genesis, God breathed it in to live on the 4th day.",
+        answer: "b"
+    },
+    {
+        question: "Which of the following can capture an element in the DOM?",
+        a: "document.getElementByID().",
+        b: "document.querySelector()",
+        c: "document.querySeletorAll()",
+        d: "All of the above",
+        answer: "d"
+    },
+    {
+        question: "What does DOM stand for?",
+        a: "Document Object Model",
+        b: "Don't Over Modulate",
+        c: "Document Outer Module",
+        d: "Dawn of Man",
+        answer: "a"
+    },
+    {
+        question: "What browswer was JavaScript originally designed for?",
+        a: "Chrome",
+        b: "Explorer",
+        c: "Netscape",
+        d: "WebCrawler",
+        answer: "c"
+    },
+    {
+        question: "What term describes the occurence of events running on parent AND children elements?",
+        a: "Bubbling",
+        b: "Legacy",
+        c: "Inheritance",
+        d: "Downcasting",
+        answer: "a"
+    },
+    {
+        question: "What method will remove white space from the beginning and end of a string?",
+        a: ".removeWhiteSpace()",
+        b: ".trim()",
+        c: ".shorten()",
+        d: ".quick()",
+        answer: "b"
+    },
+    {
+        question: "What method removes an item from the beginning of an array and returns it?",
+        a: ".pop()",
+        b: ".push()",
+        c: ".shift()",
+        d: ".unshift()",
+        answer: "c"
+    },
+    {
+        question: "What term describes the current context of a piece of code, and where it is accessible?",
+        a: "Location",
+        b: "Reachability",
+        c: "State",
+        d: "Scope",
+        answer: "d"
     }
+
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,121 @@
 const startButton = document.querySelector("#start-button");
-const quizBox = document.querySelector("#main-box");
+const mainBox = document.querySelector("#main-box");
 var timeDisplay = document.querySelector("#time-display")
 var questionText = document.querySelector("#question");
 var shownAnswers = document.querySelector("ul");
+var highScoresBtn = document.querySelector("high-scores-button");
 var currentQuestion = 0;
 var score = 0;
+var completionTime;
+var highScores;
+var highScoresScreenOn = false;
+
 
 
 var quizTime = 3;
 
-var timer = setInterval(() => {
-    timeDisplay.textContent = "Time Left: " + Math.floor(quizTime/60) + ":" + quizTime%60;   
-    quizTime--;
-        if (quizTime < 0) {
-            timeDisplay.textContent = "TIME's UP!";
-            clearInterval(timer);
-        }
 
-    }, 1000)
+function startTimer() {
+    var timer = setInterval(() => {
+        timeDisplay.textContent = "Time Left: " + Math.floor(quizTime/60) + ":" + quizTime%60;   
+        quizTime--;
+            if (quizTime <= 0) {
+                timeDisplay.textContent = "TIME's UP!";
+                endQuiz();
+                quizTime = 0;
+                clearInterval(timer);
+                
+            }
+
+        }, 1000)
+}
 
 function loadQuestion(index) {
-    questionText.textContent = questionList[index].question;
-    addAnswer('option1');
+    var newQuestion = questionList[index];
+    questionText.textContent = newQuestion.question;
+    shownAnswers.textContent = "";
+
+    // put all possible answers onthe screen.
+    // I'll have to make this into a shorter function later. DRY.
+
+    var answer1 = document.createElement('li');
+    answer1.textContent = newQuestion.a;
+    answer1.setAttribute("data-option", "a")
+    answer1.setAttribute("class", "answer")
+    shownAnswers.append(answer1);
+
+    var answer2 = document.createElement('li');
+    answer2.textContent = newQuestion.b;
+    answer2.setAttribute("data-option", "b")
+    answer2.setAttribute("class", "answer")
+    shownAnswers.append(answer2);
+
+    var answer3 = document.createElement('li');
+    answer3.textContent = newQuestion.c;
+    answer3.setAttribute("data-option", "c")
+    answer3.setAttribute("class", "answer")
+    shownAnswers.append(answer3);
+
+    var answer4 = document.createElement('li');
+    answer4.textContent = newQuestion.d;
+    answer4.setAttribute("data-option", "d")
+    answer4.setAttribute("class", "answer")
+    shownAnswers.append(answer4);
 }
 
-function addAnswer(option) {
-    var answer = document.createElement('li');
-    answer.textContent = questionList[currentQuestion].option;
-    shownAnswers.appendChild(answer);
-}
+
 
 
 
 function startQuiz() {
+    score = 0;
+    currentQuestion = 0;
+    quizTime = 10;
+    startTimer();
+    startButton.style.display = "none";
     loadQuestion(currentQuestion);
-    
-    
-
+    var scoreDisplay = document.createElement('h4');
+    scoreDisplay.textContent = "Score: " + score;
+    scoreDisplay.setAttribute('id', 'score-display');
+    mainBox.appendChild(scoreDisplay);
 }
 
 
-startButton.addEventListener("click", startQuiz);
+mainBox.addEventListener("click", (e) => {
+    if (e.target === startButton) {
+        startQuiz();
+    } else if (e.target.getAttribute('class') === 'answer') {
+        var givenAnswer = e.target.getAttribute('data-option');
+        if (givenAnswer === questionList[currentQuestion].answer) {
+            score +=10;
+            var scoreDisplay = document.getElementById('score-display');
+            scoreDisplay.textContent = "Score: " + score;
+        } else {
+            quizTime -=30;
+        }
+        currentQuestion++;
+        if (currentQuestion >= questionList.length) {
+            endQuiz();
+        } else {
+        loadQuestion(currentQuestion);
+        }
+    }
+});
+
+function endQuiz() {
+    console.log("Quiz is over!");
+    shownAnswers.textContent = "";
+    startButton.textContent = "Play Again"
+    startButton.style.display = "block";
+    completionTime = quizTime;
+    console.log(completionTime);
+}
+
+highScoresBtn.addEventListener('click', highScoreToggle);
+
+function highScoreToggle(e) {
+    if(!highScoreScreenOn) {
+        highScoresBtn.textContent = "Go Back";
+    }
+    
+}
