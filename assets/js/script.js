e9e3bb82d3be33d2ffb38e0a5e74a128a1f086ef
Ben Powell <powe0186@gmail.com>
2022-03-28 23:12:39 -0500

    created function to load question

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,37 +30,25 @@ function startTimer() {
         }, 1000)
 }
 
+
+function loadAnswer(question, answer) {
+    var newAnswer = document.createElement('li');
+    newAnswer.textContent = question[answer];
+    newAnswer.setAttribute("data-option", answer)
+    newAnswer.setAttribute("class", "answer")
+    shownAnswers.append(newAnswer);
+}
+
 function loadQuestion(index) {
     var newQuestion = questionList[index];
     questionText.textContent = newQuestion.question;
     shownAnswers.textContent = "";
-
     // put all possible answers onthe screen.
-    // I'll have to make this into a shorter function later. DRY.
 
-    var answer1 = document.createElement('li');
-    answer1.textContent = newQuestion.a;
-    answer1.setAttribute("data-option", "a")
-    answer1.setAttribute("class", "answer")
-    shownAnswers.append(answer1);
-
-    var answer2 = document.createElement('li');
-    answer2.textContent = newQuestion.b;
-    answer2.setAttribute("data-option", "b")
-    answer2.setAttribute("class", "answer")
-    shownAnswers.append(answer2);
-
-    var answer3 = document.createElement('li');
-    answer3.textContent = newQuestion.c;
-    answer3.setAttribute("data-option", "c")
-    answer3.setAttribute("class", "answer")
-    shownAnswers.append(answer3);
-
-    var answer4 = document.createElement('li');
-    answer4.textContent = newQuestion.d;
-    answer4.setAttribute("data-option", "d")
-    answer4.setAttribute("class", "answer")
-    shownAnswers.append(answer4);
+    loadAnswer(newQuestion, 'a');
+    loadAnswer(newQuestion, 'b');
+    loadAnswer(newQuestion, 'c');
+    loadAnswer(newQuestion, 'd');
 }
 
 
@@ -109,13 +97,4 @@ function endQuiz() {
     startButton.style.display = "block";
     completionTime = quizTime;
     console.log(completionTime);
-}
-
-highScoresBtn.addEventListener('click', highScoreToggle);
-
-function highScoreToggle(e) {
-    if(!highScoreScreenOn) {
-        highScoresBtn.textContent = "Go Back";
-    }
-    
 }
